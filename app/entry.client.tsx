1b96bd68503b4ab583efc3dc10234af17154c3f2
Pratham Jani <pratham@worthy.nz>
2025-07-15 15:50:48 +1200

    Update entry files for Hydrogen 2025+

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,14 +1,12 @@
-import {HydratedRouter} from 'react-router/dom';
 import {startTransition, StrictMode} from 'react';
+import {RemixBrowser} from '@remix-run/react';
 import {hydrateRoot} from 'react-dom/client';
 
-if (!window.location.origin.includes('webcache.googleusercontent.com')) {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <HydratedRouter />
-      </StrictMode>,
-    );
-  });
-}
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
+  );
+});
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,51 +1,18 @@
-import type {AppLoadContext} from '@shopify/remix-oxygen';
-import {ServerRouter} from 'react-router';
-import {isbot} from 'isbot';
-import {renderToReadableStream} from 'react-dom/server';
-import {createContentSecurityPolicy} from '@shopify/hydrogen';
-import type {EntryContext} from 'react-router';
+import type { EntryContext } from '@shopify/remix-oxygen';
+import { RemixServer } from '@remix-run/react';
+import { renderToReadableStream } from 'react-dom/server';
 
-export default async function handleRequest(
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  reactRouterContext: EntryContext,
-  context: AppLoadContext,
+  remixContext: EntryContext
 ) {
-  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
-    shop: {
-      checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
-      storeDomain: context.env.PUBLIC_STORE_DOMAIN,
-    },
-  });
-
-  const body = await renderToReadableStream(
-    <NonceProvider>
-      <ServerRouter
-        context={reactRouterContext}
-        url={request.url}
-        nonce={nonce}
-      />
-    </NonceProvider>,
+  return renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
     {
-      nonce,
+      bootstrapScripts: ['/entry.client.js'],
       signal: request.signal,
-      onError(error) {
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    },
+    }
   );
-
-  if (isbot(request.headers.get('user-agent'))) {
-    await body.allReady;
-  }
-
-  responseHeaders.set('Content-Type', 'text/html');
-  responseHeaders.set('Content-Security-Policy', header);
-
-  return new Response(body, {
-    headers: responseHeaders,
-    status: responseStatusCode,
-  });
 }
